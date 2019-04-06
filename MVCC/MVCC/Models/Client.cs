b12eb7c4da629c944e40aa95cd110ebf7ee38450
MAURO ALEJANDRO HERNANDEZ DE LAS MUÑECAS <mauroahdlm@gmail.com>
2019-04-05 22:55:12 -0400

    commit mvc completo

---
+++ MVCC/MVCC/Models/Client.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCC.Models
+{
+    public enum PayType
+    {
+        Cash,
+        CreditCard
+    }
+    
+    public class Client
+    {
+        [Key]
+        public int ClientId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int NIT { get; set; }
+
+        [Required]
+        public PayType Pay { get; set; }
+    }
+}
---
+++ MVCC/MVCC/Models/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MVCC.Models
+{
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<MVCC.Models.Client> Clients { get; set; }
+    }
+}
