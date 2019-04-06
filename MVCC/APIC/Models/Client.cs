c1da4bb2c5256458ed4aab08dbcbe61023d0bafa
MAURO ALEJANDRO HERNANDEZ DE LAS MUÑECAS <mauroahdlm@gmail.com>
2019-04-05 23:11:19 -0400

    commit completo con web api y postman

---
+++ MVCC/APIC/Models/Client.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APIC.Models
+{
+    public enum PayType
+    {
+        Cash,
+        CreditCard
+    }
+    
+    public class Client
+    {
+        [Key]
+        public int ClientId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int NIT { get; set; }
+
+        [Required]
+        public PayType Pay { get; set; }
+    }
+}
